c14ce99d397a4ecad6855c24c61a20f943395c41
marcella.justo <marcella.justo@idwall.co>
2021-07-29 22:41:49 -0300

    adicionando métodos à classe

---
+++ bytebank/src/TestaMetodo.java
@@ -0,0 +1,20 @@
+public class TestaMetodo {
+    public static void main(String[] args) {
+        Conta contaDaMarcella = new Conta();
+        contaDaMarcella.saldo = 100;
+        contaDaMarcella.deposita(75);
+
+        boolean conseguiuRetirar = contaDaMarcella.saca(35);
+
+        Conta contaDoMarcos = new Conta();
+        contaDoMarcos.deposita(1000);
+
+        if(contaDoMarcos.transfere(300, contaDaMarcella)){
+            System.out.println("Transferência feita com sucesso!");
+        }else {
+            System.out.println("Faltou dinheiro!");
+        };
+        System.out.println(contaDoMarcos.saldo);
+        System.out.println(contaDaMarcella.saldo);
+    }
+}
